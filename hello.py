5a26f890b0e18c9e4a82b9707949c7ac2894992c
c33e <95937113+c33e@users.noreply.github.com>
2022-12-08 13:34:26 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello anew from Dockerised Flask"
+    return "Hello from Caoimhes dockerised flask!!"
 
 @app.route("/route32a")
 def route32a():
